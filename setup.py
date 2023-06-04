0f4ce171b51c38d893569f66bebddde6b4d876ab
Jon Craton <jncraton@gmail.com>
2023-06-03 21:56:42 -0400

    Add project files for llama.cpp

--- setup.py
+++ setup.py
@@ -20,9 +20,5 @@ setuptools.setup(
     ],
     python_requires='>=3.8',
     install_requires=[
-        "torch",
-        "transformers",
-        "sentence-transformers",
-        "accelerate",
     ],
 )
